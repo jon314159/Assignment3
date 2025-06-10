48ad4dd975855e3139d2ceb22969658f33306c09
Jonathan Capalbo <jrc64@njit.edu>
2025-06-09 23:55:12 -0400

    Fixed faulty test operations file

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,11 +3,7 @@ from app.operations import Operations
 
 
 @pytest.mark.parametrize("a, b, expected", [
-    (2, 3, 5),
-    (0, 0, 0),
-    (-1, 1, 0),
-    (-2, -3, -5),
-    (-1, 0, -1),
+
     (2, 3, 5),
     (0, 0, 0),
     (-1, 1, 0),
